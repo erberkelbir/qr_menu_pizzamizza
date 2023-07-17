439d90bb987c12af27ae4b81615d4839695c2162
Erberk Elbir <erberkelbirr@gmail.com>
2023-07-17 19:09:40 +0300

    bu sefer we got the fastest website brom

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname + '../public')));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
+);
 
 // Define your pizza menu (pizzas, ingredients, etc.)
 
